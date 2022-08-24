a897210c9021505e17180310859191a349119a9b
Trinigv <99trini@gmail.com>
2022-08-24 19:27:34 -0300

    Change use swagger ui

--- src/index.js
+++ src/index.js
@@ -60,4 +60,4 @@ const options = {
 };
 
 const specs = swaggerJsDoc(options);
-app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(specs));
+app.use('/', swaggerUI.serve, swaggerUI.setup(specs));
