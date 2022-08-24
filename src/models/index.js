97699e368c1f63283ae7d169353b73e920a32a75
Trinigv <99trini@gmail.com>
2022-08-24 19:19:07 -0300

    Rename conflicting variable

--- src/models/index.js
+++ src/models/index.js
@@ -1,32 +1,27 @@
 const dbConfig = require('../dbConfig');
 
-const Sequelize = require('Sequelize');
+const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize(
-	dbConfig.database,
-	dbConfig.user,
-	dbConfig.password,
-	{
-		host: dbConfig.host,
-		dialect: dbConfig.dialect,
-		operationsAliases: false,
-		pool: {
-			max: dbConfig.pool.max,
-			min: dbConfig.pool.min,
-			acquire: dbConfig.pool.acquire,
-			idle: dbConfig.pool.idle,
-		},
-	}
-);
+const s = new Sequelize(dbConfig.database, dbConfig.user, dbConfig.password, {
+	host: dbConfig.host,
+	dialect: dbConfig.dialect,
+	operationsAliases: false,
+	pool: {
+		max: dbConfig.pool.max,
+		min: dbConfig.pool.min,
+		acquire: dbConfig.pool.acquire,
+		idle: dbConfig.pool.idle,
+	},
+});
 
 const db = {};
 
 db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+db.sequelize = s;
 
-db.Project = require('./project')(sequelize, Sequelize);
-db.User = require('./user')(sequelize, Sequelize);
-db.Project_User = require('./Project_User')(sequelize, Sequelize);
+db.Project = require('./project')(s, Sequelize);
+db.User = require('./user')(s, Sequelize);
+db.Project_User = require('./Project_User')(s, Sequelize);
 
 db.Project.belongsToMany(db.User, { through: 'Project_User' });
 db.User.belongsToMany(db.Project, { through: 'Project_User' });
--- src/models/project.js
+++ src/models/project.js
@@ -1,6 +1,6 @@
 const { DataTypes } = require('sequelize');
 
-module.exports = (sequelize, Sequelize) => {
+module.exports = (sequelize) => {
 	const Project = sequelize.define(
 		'Project',
 		{
