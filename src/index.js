8333c3126be152db9e35c8b2ab1220e065bd05ab
Trinigv <99trini@gmail.com>
2022-08-24 21:56:32 -0300

    Change app.use swagger

--- src/index.js
+++ src/index.js
@@ -19,27 +19,6 @@ var corsOptions = {
 };
 app.use(cors(corsOptions));
 
-db.sequelize.sync({ force: true }).then(() => {
-	app.listen(process.env.PORT || 3000, async () => {
-		console.log('Server is running on port 3000');
-		let check = await Project.findAll();
-		if (check.length === 0) {
-			await Project.bulkCreate(projects);
-			await User.bulkCreate(users);
-		}
-	});
-});
-
-// parse requests of content-type - application/json
-
-app.use(bodyParser.json());
-// parse requests of content-type - application/x-www-form-urlencoded
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//simple route
-app.use('/', indexRoutes);
-
 const options = {
 	definition: {
 		openapi: '3.0.0',
@@ -60,6 +39,27 @@ const options = {
 	},
 	apis: ['src/routes/*.js'],
 };
-
 const specs = swaggerJsDoc(options);
+
 app.use('/', swaggerUI.serve, swaggerUI.setup(specs));
+
+db.sequelize.sync({ force: true }).then(() => {
+	app.listen(process.env.PORT || 3000, async () => {
+		console.log('Server is running on port 3000');
+		let check = await Project.findAll();
+		if (check.length === 0) {
+			await Project.bulkCreate(projects);
+			await User.bulkCreate(users);
+		}
+	});
+});
+
+// parse requests of content-type - application/json
+
+app.use(bodyParser.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//simple route
+app.use('/', indexRoutes);
